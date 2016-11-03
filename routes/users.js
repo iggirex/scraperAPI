50e2583fb2053cd4eaa12186b8012c8e03c6ed73
Iggi <iggirex@gmail.com>
2016-11-03 09:57:58 -0600

    between two sites being scraped

--- routes/users.js
+++ routes/users.js
@@ -3,15 +3,52 @@ var router = express.Router();
 var cheerio = require('cheerio');
 var request = require('request')
 
-// var url = "https://news.google.com/";
+// var scrapeDataFromHtml = function(html) {
+//   var data = {};
+//   var $ = cheerio.load(html);
+//   var j = 1;
+//   $('h2').each(function() {
+//     var a = $(this);
+//     var headlines = a.children().text();
+//     console.log("this is $this: ", a[0].children[0].data)
+//     console.log("this is headline: ", headlines)
+//   })
+// };
 
+// var scrapeDataFromHtml = function(html) {
+//   var data = {};
+//   var $ = cheerio.load(html);
+//   var j = 1;
+//
+//   var placeName = $('.place');
+//   var placeNameText = placeName.text();
+//   console.log("YYYYOOOOOO ------>>>> this is placeName :",placeName)
 
-/* GET users listing. */
+  // $('.place').each(function(i, e) {
+  //   var a = $(this);
+  //   console.log("yyyyyyooooooooo THISSS is i & e:  ")
+  //
+  // })
+//   return data;
+// };
 router.get('/', function(req, res, next) {
-  request("https://news.google.com/", function(error, response, body) {
+  // request("http://www.foodspotting.com/find/best/tofu/in/Denver-CO-USA", function(error, response, body) {
+  request("http://www.pmichaud.com/toast/", function(error, response, body) {
     if(!error && response.statusCode === 200) {
-      var data = scrapeDataFromHtml(body);
-      res.send(data);
+
+      // var data = scrapeDataFromHtml(body);
+
+      var $ = cheerio.load(body);
+      var header = $('h2')
+      // var text = header.data
+      // console.log($(this))
+      console.log("THIS IS TTTEEXXXTTT :", header)
+
+      $('p').each(function() {
+        var a = $(this)
+        console.log("YYYYOOO THIS IS A!!!: ", a)
+      })
+      res.send("hi");
     }
     return console.log(error);
   })
